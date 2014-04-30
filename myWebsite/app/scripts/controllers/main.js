7434b29f450fff7f3aa8ece5f079e43e38356ce6
Hardi Bhatt <HardiBhattAtWork@gmail.com>
2014-04-30 19:11:50 -0400

    4_30_14: Yeoman Build commit

---
+++ myWebsite/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('myWebsiteApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
